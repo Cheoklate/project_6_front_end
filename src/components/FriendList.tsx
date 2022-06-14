8f1207be0ff4c0ed6069b4286adcbbb83a257bd8
smdewi <smdewi2020@gmail.com>
2022-06-14 09:21:03 +0800

    create component FriendList

---
+++ src/components/FriendList.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+
+export default function FriendList() {
+  
+  return (
+    <Typography component="h1" variant="h5">
+      Friend List
+    </Typography>
+  );
+}
