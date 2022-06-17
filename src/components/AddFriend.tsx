58cb0fd35040cb95357ed741a28a91737295312e
smdewi <smdewi2020@gmail.com>
2022-06-17 14:57:57 +0800

    add styles css for Textfield inputRounded

--- src/components/AddFriend.tsx
+++ src/components/AddFriend.tsx
@@ -1,17 +1,22 @@
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+
+import "./styles.css"; 
 
 export default function AddFriend() {
   return (
     <Box>
-      <TextField
-        className="inputRounded"
-        placeholder="Username"
-        variant="outlined"
-        size="small"
-      />
-      <Chip label="Add Friend" color="secondary" />
+      <Stack spacing={1}>
+        <TextField
+          className="inputRounded"
+          placeholder="Username"
+          variant="outlined"
+          size="small"
+        />
+        <Chip label="Add Friend" color="secondary" />
+      </Stack>
     </Box>
   );
 }
