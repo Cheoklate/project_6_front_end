3ad17008f792fe4074d082f2bc78deb1ff52e3be
smdewi <smdewi2020@gmail.com>
2022-06-22 16:29:19 +0800

    rounded Textfield and Button in Sign Up component

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import "./styles.css";
 import { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
@@ -244,128 +245,133 @@ export default function SignUp() {
 	};
 
 	return (
-		<ThemeProvider theme={theme}>
-			<Container component='main' maxWidth='xs'>
-				<CssBaseline />
-				<Box
-					sx={{
-						marginTop: 8,
-						display: 'flex',
-						flexDirection: 'column',
-						alignItems: 'center',
-					}}
-				>
-					<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-						<LockOutlinedIcon />
-					</Avatar>
-					<Typography component='h1' variant='h5'>
-						Sign up
-					</Typography>
-					<Box
-						component='form'
-						noValidate
-						onSubmit={handleSubmit}
-						sx={{ mt: 3 }}
-					>
-						<Grid container spacing={2}>
-							<Grid item xs={12} sm={6}>
-								<TextField
-									required
-									fullWidth
-									id='firstName'
-									label='First Name'
-									name='firstName'
-									autoComplete='family-name'
-									onChange={(event) => {
-										setfirstName(event.target.value);
-									}}
-								/>
-							</Grid>
-							<Grid item xs={12} sm={6}>
-								<TextField
-									required
-									fullWidth
-									id='lastName'
-									label='Last Name'
-									name='lastName'
-									autoComplete='family-name'
-									onChange={(event) => {
-										setlastName(event.target.value);
-									}}
-								/>
-							</Grid>
-							<Grid item xs={12}>
-								<TextField
-									autoComplete='username'
-									name='userName'
-									required
-									fullWidth
-									id='userName'
-									label='Username'
-									autoFocus
-									onChange={(event) => {
-										setUsername(event.target.value);
-									}}
-								/>
-							</Grid>
-							<Grid item xs={12}>
-								<TextField
-									required
-									fullWidth
-									id='email'
-									label='Email Address'
-									name='email'
-									autoComplete='email'
-									onChange={(event) => {
-										setEmail(event.target.value);
-									}}
-								/>
-							</Grid>
-							<Grid item xs={12}>
-								<TextField
-									required
-									fullWidth
-									name='password'
-									label='Password'
-									type='password'
-									id='password'
-									autoComplete='new-password'
-									onChange={(event) => {
-										setPassword(event.target.value);
-									}}
-								/>
-							</Grid>
-							<Grid item xs={12}>
-								<FormControlLabel
-									control={
-										<Checkbox value='allowExtraEmails' color='primary' />
-									}
-									label='I want to receive inspiration, marketing promotions and updates via email.'
-								/>
-							</Grid>
-						</Grid>
-						<Button
-							type='submit'
-							fullWidth
-							variant='contained'
-							sx={{ mt: 3, mb: 2 }}
-						>
-							Sign Up
-						</Button>
-						<Grid container justifyContent='flex-end'>
-							<Grid item>
-								<Link href='signin' variant='body2'>
-									Already have an account? Sign in
-								</Link>
-							</Grid>
-						</Grid>
-					</Box>
-				</Box>
-				<Typography variant='body2' color='text.secondary' align='center'>
-					<Copyright sx={{ mt: 8, mb: 4 }} component={'symbol'} />
-				</Typography>
-				<SimpleBottomNavigation/>
-			</Container>
-		</ThemeProvider>
-	);
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  className="inputRounded"
+                  id="firstName"
+                  label="First Name"
+                  name="firstName"
+                  autoComplete="family-name"
+                  onChange={(event) => {
+                    setfirstName(event.target.value);
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  className="inputRounded"
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                  onChange={(event) => {
+                    setlastName(event.target.value);
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  className="inputRounded"
+                  autoComplete="username"
+                  name="userName"
+                  required
+                  fullWidth
+                  id="userName"
+                  label="Username"
+                  autoFocus
+                  onChange={(event) => {
+                    setUsername(event.target.value);
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  className="inputRounded"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  onChange={(event) => {
+                    setEmail(event.target.value);
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  className="inputRounded"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={(event) => {
+                    setPassword(event.target.value);
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2, bgcolor: "secondary.main", borderRadius: 25 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="signin" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Typography variant="body2" color="text.secondary" align="center">
+          <Copyright sx={{ mt: 8, mb: 4 }} component={"symbol"} />
+        </Typography>
+        <SimpleBottomNavigation />
+      </Container>
+    </ThemeProvider>
+  );
 }
